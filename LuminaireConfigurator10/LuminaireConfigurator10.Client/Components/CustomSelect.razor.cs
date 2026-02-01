0da02b9a07f8f699f43d8efd687d37cacde09ee2
Emmanuel Durin <emmanueldurin@gmail.com>
2026-02-01 20:52:36 +0100

    First CustomSelect with list display

---
+++ LuminaireConfigurator10/LuminaireConfigurator10.Client/Components/CustomSelect.razor.cs
@@ -0,0 +1,13 @@
+﻿using LuminaireConfigurator10.Client.ViewModel;
+using LuminaireConfigurator10.Client.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace LuminaireConfigurator10.Client.Components
+{
+    public partial class CustomSelect
+    {
+        [Parameter]
+        public IEnumerable<string> Items { get; set; } = Enumerable.Empty<string>();
+    }
+}
