60ed19dd6a39d13e29b1d4c12c132f414574f6ff
Emmanuel Durin <emmanueldurin@gmail.com>
2026-02-01 18:55:30 +0100

    Implement Form with Model

---
+++ LuminaireConfigurator10/LuminaireConfigurator10.Client/Pages/ConfigurationCreation.razor.cs
@@ -0,0 +1,17 @@
+﻿using LuminaireConfigurator10.Client.ViewModel;
+using LuminaireConfigurator10.Client.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace LuminaireConfigurator10.Client.Pages
+{
+    public partial class ConfigurationCreation
+    {
+        public LuminaireConfiguration Configuration { get; set; } = new();
+        public void Create()
+        {
+            Console.WriteLine("configuration created");
+        }
+        public int[] LampColors { get; set; } = new int[]{2200,2700,3000,4000,5700};
+        public string[] Optics { get; set; } = new string[]{"ON10","ON11","OL10","OL11","OM10","OM11"};
+    }
+}
---
+++ LuminaireConfigurator10/LuminaireConfigurator10.Client/ViewModel/LuminaireConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuminaireConfigurator10.Client.ViewModel
+{
+    public class LuminaireConfiguration
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [Range(1,1E10)]
+        public double LampFlux { get; set; }
+        public decimal Price { get; set; }
+        [Required]
+        public string? Optic { get; set; }
+        [Required]
+        [Range(1,1E6)]
+        public int LampColor { get; set; }
+    }
+}
