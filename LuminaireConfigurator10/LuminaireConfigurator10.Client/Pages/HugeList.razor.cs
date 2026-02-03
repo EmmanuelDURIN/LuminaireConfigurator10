6d72fcf6110f5296445144858574f6a1d27d9da5
Emmanuel Durin <emmanueldurin@gmail.com>
2026-02-03 08:12:48 +0100

    HugeList without virtualization

---
+++ LuminaireConfigurator10/LuminaireConfigurator10.Client/Pages/HugeList.razor.cs
@@ -0,0 +1,33 @@
+﻿using LuminaireConfigurator10.Client.Model;
+
+namespace LuminaireConfigurator10.Client.Pages
+{
+  public partial class HugeList
+  {
+    private LuminaireConfiguration[]? luminaireConfigurations;
+    public LuminaireConfiguration[]? LuminaireConfigurations
+    {
+      get => luminaireConfigurations;
+      set => luminaireConfigurations = value;
+    }
+    protected override Task OnInitializedAsync()
+    {
+            Console.WriteLine("Before Enumerable.Range");
+      LuminaireConfigurations = Enumerable.Range(1, 500_000)
+                                          .Select(
+                                            i => new LuminaireConfiguration
+                                            {
+                                              Id = i,
+                                              Name = "Luminaire" + i,
+                                              CreationTime = DateTime.Now,
+                                              LampColor = 3000 + (i % 2000),
+                                              Optic = "OM" + (i % 10),
+                                              LampFlux = 1000,
+                                            })
+                                          .ToArray();
+      Console.WriteLine("After Enumerable.Range");
+
+      return Task.FromResult(0);
+    }
+  }
+}
